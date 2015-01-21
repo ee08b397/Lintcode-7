dca86235496499a4b5d5934bcc72691918ea88ec
Lawrence <jiangyi0425@gmail.com>
2015-01-21 11:17:05 +0800

    Cpp

---
+++ Cpp/A + B Problem.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /*
+     * param a: The first integer
+     * param b: The second integer
+     * return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        while(b != 0){
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+};

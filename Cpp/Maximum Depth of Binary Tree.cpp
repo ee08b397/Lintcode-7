9047706616a99861c1946dbc021680e338d4a34d
Lawrence <jiangyi0425@gmail.com>
2015-01-21 11:08:32 +0800

    Cpp

---
+++ Cpp/Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition of TreeNode:
+ */
+class TreeNode {
+public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int val) {
+        this->val = val;
+        this->left = this->right = NULL;
+    }
+};
+
+class Solution {
+public:
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+    int maxDepth(TreeNode *root) {
+        if(root == NULL) return 0;
+        else return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        // write your code here
+    }
+};

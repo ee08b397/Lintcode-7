1d4be1c770ed516f37e460d9725d4d4ddab33131
Lawrence <jiangyi0425@gmail.com>
2015-05-21 12:07:44 +0800

    Cpp

--- Cpp/Sliding Window Maximum.cpp
+++ Cpp/Sliding Window Maximum.cpp
@@ -1,3 +1,4 @@
+#include <deque>
 #include <vector>
 
 class Solution {
@@ -6,47 +7,30 @@ public:
      * @param nums: A list of integers.
      * @return: The maximum number inside the window at each moving.
      */
-    class SegmentTreeNode {
-    public:
-        int start, end, val;
-        SegmentTreeNode *left, *right;
-        SegmentTreeNode(int start, int end, int val) {
-            this->start = start, this->end = end, this->val = val;
-            this->left = this->right = NULL;
-        }
-    };
-    SegmentTreeNode *build(vector<int> &nums, int start, int end) {
-        if (start > end) return NULL;
-        if (start == end) {
-            SegmentTreeNode* root = new SegmentTreeNode(start, end, nums[start]);
-            return root;
-        }
-        int mid = start + (end - start) / 2;
-        SegmentTreeNode *root = new SegmentTreeNode(start, end, nums[mid]);
-        root->left = build(nums, start, mid);
-        root->right = build(nums, mid + 1, end);
-        root->val = max(root->left->val, root->right->val);
-        return root;
-    }
-    int query(int start, int end, SegmentTreeNode *root) {
-        if (start <= root->start && end >= root->end) return root->val;
-        else {
-            int mid = root->start + (root->end - root->start) / 2;
-            if (start > mid) return query(start, end, root->right);
-            else if (end <= mid) return query(start, end, root->left);
-            else {
-                return max(query(start, mid, root->left), query(mid + 1, end, root->right));
-            }
-        }
-    }
     vector<int> maxSlidingWindow(vector<int> &nums, int k) {
         // write your code here
-        SegmentTreeNode *root = build(nums, 0, nums.size() - 1);
-        vector<int> vec;
-        for (int i = 0; i + k - 1 < nums.size(); i ++) {
-            vec.push_back(query(i, i + k - 1, root));
+        vector<int> res;
+        if (k == 0 || nums.empty()) return res;
+        deque<int> deq;
+        for (int i = 0; i < k; i ++) {
+            while (!deq.empty() && nums[i] >= nums[deq.back()]) {
+                deq.pop_back();
+            }
+            deq.push_back(i);
         }
-        return vec;
+        int len = nums.size();
+        for (int i = k; i < len; i ++) {
+            res.push_back(nums[deq.front()]);
+            while (!deq.empty() && nums[i] >= nums[deq.back()]) {
+                deq.pop_back();
+            }
+            while (!deq.empty() && deq.front() <= i - k) {
+                deq.pop_front();
+            }
+            deq.push_back(i);
+        }
+        res.push_back(nums[deq.front()]);
+        return res;
     }
 };
 

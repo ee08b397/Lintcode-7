a9c5997abc5fa550fa30f780ec23a91067111ea8
Lawrence <jiangyi0425@gmail.com>
2015-05-28 23:57:43 +0800

    Cpp

---
+++ Cpp/Count and Say.cpp
@@ -0,0 +1,37 @@
+#include <string>
+
+class Solution {
+public:
+    /**
+     * @param n the nth
+     * @return the nth sequence
+     */
+    string solve(const string &str) {
+        int num = 1;
+        char ch = str[0];
+        string st = "";
+        for (int i = 1; i < str.length(); i ++) {
+            if (str[i] == ch) {
+                num ++;
+            } else {
+                st += to_string(num);
+                st += str[i-1];
+                num = 1;
+                ch = str[i];
+            }
+        }
+        st += to_string(num);
+        st += str.back();
+        return st;
+    }
+    string countAndSay(int n) {
+        // Write your code here
+        string str = "1";
+        n -= 1;
+        while (n > 0) {
+            string st = solve(str);
+            str = st;
+        }
+        return str;
+    }
+};

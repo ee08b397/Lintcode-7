5939d993b9aa8b3f9ca991cb7061ec0b4a860baf
Lawrence <jiangyi0425@gmail.com>
2015-06-11 23:30:48 +0800

    Cpp

--- Cpp/Rectangle Area.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int sums = (C - A) * (D - B) + (G - E) * (H - F);
-        return sums - max(min(C, G) - max(A, E), 0) * max(min(D, H) - max(B, F), 0);
-    }
-};

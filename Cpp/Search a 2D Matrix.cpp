8d466f35c3111e90a3289b0cf44df8b8611f6d47
Lawrence <jiangyi0425@gmail.com>
2015-06-20 14:33:19 +0800

    Cpp

--- Cpp/Search a 2D Matrix.cpp
+++ Cpp/Search a 2D Matrix.cpp
@@ -7,14 +7,18 @@ public:
      */
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
         // write your code here
-        if (matrix.size() == 0) return false;
+        if (matrix.empty() || matrix[0].empty()) return false;
         int m = matrix.size(), n = matrix[0].size();
-        int x = 0, y = n - 1;
-        while (x < m && y >= 0) {
-            if (matrix[x][y] == target) return true;
-            else if(matrix[x][y] > target) y --;
-            else x ++;
+        int start = 0, end = m * n - 1, mid;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            int cur = matrix[mid / n][mid % n];
+            if (cur == target) return true;
+            else if (cur > target) end = mid;
+            else start = mid;
         }
+        if (matrix[start / n][start % n] == target) return true;
+        else if (matrix[end / n][end % n] == target) return true;
         return false;
     }
 };

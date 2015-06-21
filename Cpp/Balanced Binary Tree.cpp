cf175ca5232a8836ef07031d89f735a81225a103
Lawrence <jiangyi0425@gmail.com>
2015-06-21 15:12:28 +0800

    Cpp

--- Cpp/Balanced Binary Tree.cpp
+++ Cpp/Balanced Binary Tree.cpp
@@ -17,14 +17,14 @@ public:
      * @param root: The root of binary tree.
      * @return: True if this Binary tree is Balanced, or false.
      */
-    int dfs(TreeNode *root) {
+    int getdepth(TreeNode *root) {
         if (root == NULL) return 0;
-        else return max(dfs(root->left), dfs(root->right)) + 1;
+        int left = getdepth(root->left);
+        int right = getdepth(root->right);
+        if (left == -1 || right == - 1 || abs(left - right) > 1) return -1;
+        return max(left, right) + 1;
     }
     bool isBalanced(TreeNode *root) {
         // write your code here
-        if (root == NULL) return true;
-        if (abs(dfs(root->left) - dfs(root->right)) > 1) return false;
-        else return isBalanced(root->left) && isBalanced(root->right);
     }
 };

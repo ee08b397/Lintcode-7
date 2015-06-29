e5724232f609dd3f09ae14db14a71f4237a47f00
Lawrence <jiangyi0425@gmail.com>
2015-06-29 19:12:00 +0800

    Cpp

--- Cpp/Triangle.cpp
+++ Cpp/Triangle.cpp
@@ -7,15 +7,17 @@ public:
     int minimumTotal(vector<vector<int> > &triangle) {
         // write your code here
         int len = triangle.size();
-        vector <int> vec(len);
+        if (len == 0 || triangle[0].empty()) return 0;
+        vector<int> dp(len);
         for (int i = 0; i < len; i ++) {
-            vec[i] = triangle[len-1][i];
+        	dp[i] = triangle[len - 1][i];
         }
-        for (int i = len-2; i >= 0; i --) {
-            for (int j = 0; j <= i; j ++) {
-                vec[j] = min(vec[j] + triangle[i][j], vec[j+1] + triangle[i][j]);
-            }
+        for (int i = len - 2; i >= 0; i --) {
+        	for (int j = 0; j <= i; j ++) {
+        		dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j];
+        	}
         }
-        return vec[0];
+        return dp[0];
     }
 };
+

073db0c7cacc9b48c3695bfc25a2ae53eb7d46ce
Lawrence <jiangyi0425@gmail.com>
2015-06-29 19:13:39 +0800

    Java

--- Java/Climbing-Stairs.java
+++ Java/Climbing-Stairs.java
@@ -7,8 +7,9 @@ public class Solution {
         // write your code here
         if (n <= 3) return n;
         int[] arr = new int[n];
-        arr[0] = 1, arr[1] = 2;
-        for (int i = 3; i < n; i ++) {
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i < n; i ++) {
         	arr[i] = arr[i - 1] + arr[i - 2];
         }
         return arr[n - 1];

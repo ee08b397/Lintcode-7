78e1b451f855019c39a07b3a0dbf751b40398bfc
Lawrence <jiangyi0425@gmail.com>
2015-06-29 19:32:02 +0800

    Java

---
+++ Java/Compare-Strings.java
@@ -0,0 +1,21 @@
+public class Solution {
+    /**
+     * @param A : A string includes Upper Case letters
+     * @param B : A string includes Upper Case letter
+     * @return :  if string A contains all of the characters in B return true else return false
+     */
+    public boolean compareStrings(String A, String B) {
+        // write your code here
+        int []vis = new int[26];
+        for (int i = 0; i < A.length(); i ++) {
+        	int temp = (int)(A.charAt(i) - 'A');
+        	vis[temp] ++;
+        }
+        for (int i = 0; i < B.length(); i ++) {
+        	int temp = (int)(B.charAt(i) - 'A');
+        	if (vis[temp] == 0) return false;
+        	vis[temp] --;
+        }
+        return true;
+    }
+}

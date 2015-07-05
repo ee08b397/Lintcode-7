303c7e7e24e44f812c23fbe5fd43a39174c504c3
Lawrence <jiangyi0425@gmail.com>
2015-07-05 23:58:05 +0800

    Java

--- Java/Insertion-Sort-List.java
+++ Java/Insertion-Sort-List.java
@@ -18,10 +18,21 @@ public class Solution {
         // write your code here
         if (head == null || head.next == null) return head;
         ListNode dummy = new ListNode(-1);
-        dummy.next = head;
         ListNode cur = dummy;
-        while (cur.next != null) {
-        	ListNode temp = cur.next;
+        cur.next = head;
+        head = head.next;
+        dummy.next.next = null;
+        while (head != null) {
+        	ListNode temp = dummy;
+        	ListNode cnt = head.next;
+        	while (temp.next != null && temp.next.val < head.val) {
+        		temp = temp.next;
+        	}
+        	ListNode p = temp.next;
+        	temp.next = head;
+        	head.next = p;
+        	head = cnt;
         }
-    } 
+        return dummy.next;
+    }
 }

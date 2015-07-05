8047da92fef6ff357b62bf2d1bfd83aedb2633fa
Lawrence <jiangyi0425@gmail.com>
2015-07-05 21:40:53 +0800

    Java

---
+++ Java/Linked-List-Cycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {  
+        // write your code here
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+        	slow = slow.next;
+        	fast = fast.next.next;
+        	if (slow == fast) return true;
+        }
+        return false;
+    }
+}
+

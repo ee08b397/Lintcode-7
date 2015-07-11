f701578567cfd7ee960e76d417a46816759c861b
Lawrence <jiangyi0425@gmail.com>
2015-07-11 12:07:32 +0800

    Java

--- Java/Majority-Number-II.java
+++ Java/Majority-Number-II.java
@@ -5,6 +5,30 @@ public class Solution {
      */
     public int majorityNumber(ArrayList<Integer> nums) {
         // write your code
+        int cur1 = 0, num1 = 0, cur2 = 0, num2 = 0;
+        for (int i = 0; i < nums.size(); i ++) {
+            int cnt = nums.get(i);
+            if (num1 != 0 && cur1 == cnt) {
+                num1 ++;
+            } else if (num2 != 0 && cur2 == cnt) {
+                num2 ++;
+            } else if (num1 == 0) {
+                cur1 = cnt;
+                num1 = 1;
+            } else if (num2 == 0) {
+                cur2 = cnt;
+                num2 = 1;
+            } else {
+                num1 --;
+                num2 --;
+            }
+        }
+        num1 = 0; num2 = 0;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (nums.get(i) == cur1) num1 ++;
+            else if (nums.get(i) == cur2) num2 ++;
+        }
+        if (num1 > num2) return cur1;
+        else return cur2;
     }
 }
-

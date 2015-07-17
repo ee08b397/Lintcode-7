d9b1cce9c20f17c143a1c154d59eeae685b4e96b
Lawrence <jiangyi0425@gmail.com>
2015-07-17 20:50:14 +0800

    Java

---
+++ Java/Ugly-Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param k: The number k.
+     * @return: The kth prime number as description.
+     */
+    public long kthPrimeNumber(int k) {
+        // write your code here
+    }
+};
+

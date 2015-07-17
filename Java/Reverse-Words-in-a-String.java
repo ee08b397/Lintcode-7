e8606cddb399b5028a252cb7cd9b81949e52495c
Lawrence <jiangyi0425@gmail.com>
2015-07-17 20:18:15 +0800

    Cpp

--- Java/Reverse-Words-in-a-String.java
+++ Java/Reverse-Words-in-a-String.java
@@ -5,6 +5,20 @@ public class Solution {
      */
     public String reverseWords(String s) {
         // write your code
+        String stringarray[] = s.split(" ");
+        StringBuffer sb = new StringBuffer();
+        int sign = 0;
+        for (int i = stringarray.length - 1; i >= 0; i --) {
+            if (stringarray[i] != " ") {
+                if (sign == 0) {
+                    sb.append(stringarray[i]);
+                    sign = 1;
+                }else {
+                    sb.append(" ");
+                    sb.append(stringarray[i]);
+                }
+            }
+        }
+        return sb.toString();
     }
 }
-

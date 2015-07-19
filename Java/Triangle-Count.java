291173f2c09370ba9d01b8e5816cefef71c45d5d
Lawrence <jiangyi0425@gmail.com>
2015-07-19 23:46:29 +0800

    Java

--- Java/Triangle-Count.java
+++ Java/Triangle-Count.java
@@ -5,6 +5,20 @@ public class Solution {
      */
     public int triangleCount(int S[]) {
         // write your code here
+        Arrays.sort(S);
+        int res = 0;
+        for (int i = S.length - 1; i - 2 >= 0; i --) {
+            int left = 0, right = i - 1;
+            while (left < right) {
+                int temp = S[left] + S[right];
+                if (temp <= S[i]) {
+                    left ++;
+                } else {
+                    res += (right - left);
+                    right --;
+                }
+            }
+        }
+        return res;
     }
 }
-

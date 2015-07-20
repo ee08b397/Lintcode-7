569b719d69b85d56bd33cd9511bb28a467b281d1
Lawrence <jiangyi0425@gmail.com>
2015-07-20 10:16:21 +0800

    Java

---
+++ Java/Linked-List-Cycle-II.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins. 
+     *           if there is no cycle, return null
+     */
+    public ListNode detectCycle(ListNode head) {  
+        // write your code here
+    }
+}
+

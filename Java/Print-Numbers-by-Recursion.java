6f3abe4007b15b98bd28ab45901b89258704e555
Lawrence <jiangyi0425@gmail.com>
2015-07-26 00:01:24 +0800

    Java

---
+++ Java/Print-Numbers-by-Recursion.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /**
+     * @param n: An integer.
+     * return : An array storing 1 to the largest number with n digits.
+     */
+    public List<Integer> numbersByRecursion(int n) {
+        // write your code here
+    }
+}
--- Java/Unique-Binary-Search-Trees.java
+++ Java/Unique-Binary-Search-Trees.java
@@ -5,5 +5,16 @@ public class Solution {
      */
     public int numTrees(int n) {
         // write your code here
+        if (n <= 1) return 1;
+        int[] catalan = new int[n + 1];
+        catalan[0] = catalan[1] = 1;
+        for (int i = 2; i <= n; i ++) {
+            int cur = 0;
+            for (int j = 0; j < i; j ++) {
+                cur += catalan[j] * catalan[i - 1 - j];
+            }
+            catalan[i] = cur;
+        }
+        return catalan[n];
     }
 }

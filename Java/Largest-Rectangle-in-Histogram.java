a165e4710903680fcb0226fad5ff2dac947d7f03
Lawrence <jiangyi0425@gmail.com>
2015-07-26 11:37:15 +0800

    Java

--- Java/Largest-Rectangle-in-Histogram.java
+++ Java/Largest-Rectangle-in-Histogram.java
@@ -5,6 +5,18 @@ public class Solution {
      */
     public int largestRectangleArea(int[] height) {
         // write your code here
+        if (height == null || height.length == 0) return 0;
+        Stack<Integer> stk = new Stack<Integer>();
+        int maxArea = 0;
+        for (int i = 0; i <= height.length; i ++) {
+            int curt = (i == height.length) ? -1 : height[i];
+            while (!stk.isEmpty() && curt <= height[stk.peek()]) {
+                int h = height[stk.pop()];
+                int w = stk.isEmpty() ? i : i - stk.peek() - 1;
+                maxArea = Math.max(maxArea, h * w);
+            }
+            stk.push(i);
+        }
+        return maxArea;
     }
 }
-

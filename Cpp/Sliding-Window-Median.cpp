d5883cc3c15195d76130bf06ff565369852b0cb2
Lawrence <jiangyi0425@gmail.com>
2015-07-27 19:39:13 +0800

    Cpp

--- Cpp/Sliding-Window-Median.cpp
+++ Cpp/Sliding-Window-Median.cpp
@@ -1,14 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <set>
-
-/**
- * 本代码由九章算法编辑提供。没有版权欢迎转发。
- * - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
- * - 现有的面试培训课程包括：九章算法班，系统设计班，BAT国内班
- * - 更多详情请见官方网站：http://www.jiuzhang.com/
- */
-
 class Solution {
 public:
     /**

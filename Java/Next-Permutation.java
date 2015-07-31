1ac85df397eed756ddbc895be88c6acd2c5492ae
Lawrence <jiangyi0425@gmail.com>
2015-07-31 17:07:30 +0800

    Java

---
+++ Java/Next-Permutation.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: return nothing (void), do not return anything, modify nums in-place instead
+     */
+    public boolean hash_permutation(int[] nums) {
+        int i = nums.length - 1, i1 = i - 1, i2 = i;
+        while (i1 >= 0 && nums[i1] >= nums[i]) {
+            i--;
+            i1--;
+        }
+        if (i1 < 0) return false;
+        while (i2 >= 0 && nums[i2] <= nums[i1]) {
+            i2 --;
+        }
+        int temp = nums[i1];
+        nums[i1] = nums[i2];
+        nums[i2] = temp;
+        int left = i, right = nums.length - 1;
+        while (left < right) {
+            temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left ++;
+            right --;
+        }
+        return true;
+    }
+    public int[] nextPermutation(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length <= 1) return nums;
+        if (hash_permutation(nums)) return nums;
+        Arrays.sort(nums);
+        return nums;
+    }
+}

d7e12979b404524b8e165ef82958a043c8a76fd1
Lawrence <jiangyi0425@gmail.com>
2015-08-01 19:26:30 +0800

    Java

--- Java/Linked-List-Cycle-II.java
+++ Java/Linked-List-Cycle-II.java
@@ -15,8 +15,29 @@ public class Solution {
      * @return: The node where the cycle begins. 
      *           if there is no cycle, return null
      */
-    public ListNode detectCycle(ListNode head) {  
+    public ListNode detectCycle(ListNode head) {
         // write your code here
+        if (head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        int len = 1;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            len ++;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null;
+        ListNode cur = head;
+        slow = head;
+        for (int i = 0; i < len; i ++) {
+            cur = cur.next;
+        }
+        while (slow != cur) {
+            slow = slow.next;
+            cur = cur.next;
+        }
+        return cur;
     }
 }
 

5f81fe5e48fcec5beefe915dcb7b6c3492d0cefc
Lawrence <jiangyi0425@gmail.com>
2015-08-10 21:52:12 +0800

    Java

--- Java/Linked-List-Cycle-II.java
+++ Java/Linked-List-Cycle-II.java
@@ -1,43 +1,39 @@
 /**
- * Definition for ListNode.
- * public class ListNode {
+ * Definition of ListNode
+ * class ListNode {
+ * public:
  *     int val;
- *     ListNode next;
+ *     ListNode *next;
  *     ListNode(int val) {
- *         this.val = val;
- *         this.next = null;
+ *         this->val = val;
+ *         this->next = NULL;
  *     }
  * }
- */ 
-public class Solution {
+ */
+class Solution {
+public:
     /**
      * @param head: The first node of linked list.
-     * @return: The node where the cycle begins. 
+     * @return: The node where the cycle begins.
      *           if there is no cycle, return null
      */
-    public ListNode detectCycle(ListNode head) {
+    ListNode *detectCycle(ListNode *head) {
         // write your code here
-        if (head == null || head.next == null) return null;
-        ListNode slow = head;
-        ListNode fast = head.next;
-        int len = 1;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            len ++;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
             if (slow == fast) break;
         }
-        if (fast == null || fast.next == null) return null;
-        ListNode cur = head;
+        if (fast == NULL || fast->next == NULL) return NULL;
         slow = head;
-        for (int i = 0; i < len; i ++) {
-            cur = cur.next;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
         }
-        while (slow != cur) {
-            slow = slow.next;
-            cur = cur.next;
-        }
-        return cur;
+        return slow;
     }
-}
+};
+
 

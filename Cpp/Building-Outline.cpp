3660956388f54424ab8fd97d630b4a9fea20b82a
Lawrence <jiangyi0425@gmail.com>
2015-08-16 18:18:58 +0800

    Cpp

---
+++ Cpp/Building-Outline.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param buildings: A list of lists of integers
+     * @return: Find the outline of those buildings
+     */
+    vector<vector<int> > buildingOutline(vector<vector<int> > &buildings) {
+        // write your code here
+    }
+};
